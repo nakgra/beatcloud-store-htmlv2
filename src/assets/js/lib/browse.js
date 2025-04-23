043af35530f0ac0cc5fa055bc14d016f06eb3e6e
nakgra <nak@grantech.co.jp>
2025-04-23 15:08:59 +0900

    商品検索条件表示操作用JS

--- src/assets/js/lib/browse.js
+++ src/assets/js/lib/browse.js
@@ -15,14 +15,26 @@ class Browse {
     #orderByButton
     #orderByMenu
 
+    #filterModal
+    #filterModalBody
+    #filterModalTrigger
+    #filterModalCloseButton
+
     constructor(element, targetAttrName = null) {
         let obj = this;
-
         obj.element = element;
         obj.myAttrName = targetAttrName ? targetAttrName : obj.constructor.targetAttrName;
 
+        obj.init();
+
+    }
+
+    init() {
+        let obj = this;
+
         obj.headerNav = obj.element.querySelector('[' + obj.getMyAttrName('-nav') + ']');
 
+        // フォーム要素の選択時に送信
         Array.prototype.forEach.call(obj.element.elements, function(formControl) {
             formControl.addEventListener('change', function(e) {
                 if (Foundation.MediaQuery.atLeast('large')) {
@@ -30,6 +42,19 @@ class Browse {
                 }
             });
         });
+
+        obj.initOrderby();
+
+        obj.initFilterModal();
+
+        obj.resize();
+    }
+
+    /**
+     *
+     **/
+    initOrderby() {
+        let obj = this;
 
         // 並び順
         obj.orderByContainer = obj.element.querySelector('[' + obj.getMyAttrName('-orderby') + ']');
@@ -50,7 +75,128 @@ class Browse {
                 func.removeClass(obj.orderByMenu, '-open');
             }
         });
+    }
 
+    /**
+     *
+     **/
+    initFilterModal() {
+        let obj = this;
+
+        //モーダル親
+        obj.filterModal  = document.querySelector('[' + obj.getMyAttrName('-filter') + ']');
+
+        // モーダル要素コンテナ
+        obj.filterModalBody  = document.querySelector('[' + obj.getMyAttrName('-filter-container') + ']');
+
+        // 絞り込みボタン
+        obj.filterModalTrigger  = document.querySelector('[' + obj.getMyAttrName('-filter-button') + ']');
+        if (!obj.filterModalTrigger) {
+            return;
+        }
+
+        obj.filterModalTrigger.addEventListener('click', function(e) {
+            e.stopPropagation();
+
+            obj.closeOthers();
+            obj.openModal();
+        });
+
+        // 検索ポップアップ閉じる
+        obj.filterModalCloseButton  = document.querySelector('[' + obj.getMyAttrName('-filter-close') + ']');
+        if (obj.filterModalCloseButton) {
+            obj.filterModalCloseButton.addEventListener('click', function(e) {
+                e.preventDefault();
+                // e.stopPropagation();
+                obj.closeModal();
+            });
+        }
+
+        //escキー
+        document.body.addEventListener('keydown', function(e) {
+            if (e.keyCode == 27 && obj.isOpen()) {
+                obj.close();
+            }
+        });
+
+        //ウィンドウのリサイズ
+        $(window).on('changed.zf.mediaquery', function(event, newSize, oldSize) {
+            // newSize is the name of the now-current breakpoint, oldSize is the previous breakpoint
+            obj.resize();
+        });
+
+        // カスタムイベント
+        obj.filterModal.addEventListener('closeRequested', function(e) {
+            obj.closeModal();
+        });
+    }
+
+    /**
+     *
+     **/
+    resize() {
+        let obj = this;
+
+        if (Foundation.MediaQuery.is('large up')) {
+            if (obj.isModalOpen()) {
+                obj.closeModal();
+            }
+
+            obj.headerNav.appendChild(obj.orderByContainer);
+            func.removeClass(obj.orderByContainer, '-filter');
+        } else {
+            func.removeClass(obj.orderByMenu, '-open');
+
+            obj.filterModalBody.prepend(obj.orderByContainer);
+            func.addClass(obj.orderByContainer, '-filter');
+        }
+    }
+
+    /**
+     *
+     **/
+    isModalOpen() {
+        let obj = this;
+
+        return obj.filterModal.hasAttribute('data-opened');
+    }
+
+    /**
+     *
+     **/
+    openModal() {
+        let obj = this;
+
+        // document.body.style.top = `-${window.scrollY}px`;
+        obj.filterModal.setAttribute('data-opened', '');
+        func.addClass(document.documentElement, '-overlay');
+    }
+
+    /**
+     *
+     **/
+    closeModal() {
+        let obj = this;
+        // const scrollY = document.body.style.top;
+
+        obj.filterModal.removeAttribute('data-opened');
+        func.removeClass(document.documentElement, '-overlay');
+        // document.body.style.top = '';
+        // window.scrollTo(0, parseInt(scrollY || '0') * -1);
+    }
+
+    /**
+     *
+     **/
+    closeOthers() {
+        let obj = this;
+        let event = new Event('closeRequested')
+        let others = document.querySelectorAll('[data-opened]');
+        Array.prototype.forEach.call(others, function(other) {
+            if (other != obj.filterModal) {
+                other.dispatchEvent(event);
+            }
+        });
     }
 
     getMyAttrName(additional = '') {
